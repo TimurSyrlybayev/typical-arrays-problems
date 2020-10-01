242ab29cd89883733d4fc2f06dd4ba48b8ddbdb2
Timur Syrlybayev <timur.syrlybayev@gmail.com>
2020-10-01 16:19:19 +0600

    init: start and finish typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (array === undefined) {
+    return 0;
+  }
+  let minNumber = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (minNumber < array[i]) {
+      minNumber;
+    } else {
+      minNumber = array[i];
+    }
+  }
+  return minNumber;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (array === undefined) {
+    return 0;
+  }
+  let maxNumber = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (maxNumber > array[i]) {
+      maxNumber;
+    } else {
+      maxNumber = array[i];
+    }
+  }
+  return maxNumber;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (array === undefined || array.length === 0) {
+    return 0;
+  }
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  return sum / array.length;
 }
